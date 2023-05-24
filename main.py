b77e99d6ba914318f562516025cb22977df6d96d
MakoveevDmitriy <131581608+MakoveevDmitriy@users.noreply.github.com>
2023-05-24 12:29:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+import os
+import random
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='-', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def mem(ctx):
+    images = os.listdir('memas')
+
+    img_name = random.choice(images)
+
+    with open(f'memas/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+
+
+bot.run("токен")
